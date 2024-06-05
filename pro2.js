659ec6fb017d903621bc890130acfee9b1372f7d
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-06-05 08:30:15 +0100

    .

---
+++ pro2.js
@@ -0,0 +1,10 @@
+/*QUESTION 1
+What does this code log to the console? Does executing
+ the foo function affect the output? Why or why not? 
+
+let bar = 1;
+function foo() {
+let bar = 2
+}
+foo();
+console.log(bar);*/
