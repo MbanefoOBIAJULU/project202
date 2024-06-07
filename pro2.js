64a4a5fc387700706defd9b62dbdb6c53884d8d6
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-06-07 19:02:37 +0100

    .

--- pro2.js
+++ pro2.js
@@ -1,5 +1,5 @@
 
-function multiply(left, right) {
+/*function multiply(left, right) {
     let product = left * right; //product
     return product;
     }
@@ -9,3 +9,16 @@ function multiply(left, right) {
     let left = getNumber('Enter the first number: '); //left
     let right = getNumber('Enter the second number: ');//right
     console.log(`${left} * ${right} = ${multiply(left, right)}`);
+
+bar = 2;    
+let bar = 1;
+//function foo() 
+//foo();
+console.log(bar);
+
+In the exercises for the previous variable session,
+you wrote a dynamic greeter program named greeter.js. 
+Add a function to this program that solicits the user's
+first and last names in separate invocations; the function
+should return the appropriate name as a string. Use the
+return values to greet the user with their full name.*/
