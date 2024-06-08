49995a975ba74e35745666553ad9006783aaff7b
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-06-08 10:26:45 +0100

    pro2.js

---
+++ pro2.js
@@ -0,0 +1,14 @@
+
+/*What does the following code log to the console?
+function scream(words) {
+words = words + '!!!!';
+return;
+console.log(words);
+}
+scream('Yipeee');*/
+
+function scream(words) {
+  return words + '!!!!';
+  }
+  
+  scream('Yipeee');//..
